f44e520a4b14ac06a2ed990bcfe81486d25fcd92
maikirakiwi <stella@mai.kiwi>
2022-07-06 00:21:42 -0700

    katatsuki attempt

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use neon::prelude::*;
 use lofty::{Accessor, AudioFile, Probe, FileProperties};
+use katatsuki::{Track};
 use std::{path::Path, ffi::OsStr};
 extern crate neon;
 extern crate base64;
@@ -16,37 +17,59 @@ pub fn parse_file(mut cx: FunctionContext) -> JsResult<JsObject> {
     let arg: Handle<JsString> = cx.argument(0)?; 
     let path_str: String = arg.value(&mut cx);
     let path: &Path = Path::new(&path_str);
-
-    let tagged_file = Probe::open(path)
-		.expect("ERROR: Bad path provided!")
-		.read(true)
-		.expect("ERROR: Failed to read file!");
-
-    let tag = match tagged_file.primary_tag() {
-		Some(primary_tag) => primary_tag,
-		// If the "primary" tag doesn't exist, we just grab the
-		// first tag we can find. Realistically, a tag reader would likely
-		// iterate through the tags to find a suitable one.
-		None => tagged_file.first_tag().expect("ERROR: No tags found!"),
-	};
-
-    let properties: &FileProperties = tagged_file.properties();
-    let duration: u128 = properties.duration().as_millis();
-    //let picture: &[Picture] = tag.pictures();
     let file_ext = path.extension().and_then(OsStr::to_str);
+
+    if file_ext.to_ascii_lowercase() == "aac" | "m4a" { 
+      let tagged_file: Result<Track, std::io::Error> = Track::from_path(path, Option::from(_));
+      let tag: Track = tagged_file.unwrap();
+      let duration: i32 = tag.duration() * 1000;
+
+      let bitrate: Handle<JsNumber> = cx.number(tag.bitrate().unwrap_or(0));
+      let sample_rate: Handle<JsNumber> = cx.number(tag.sample_rate().unwrap_or(0));
+      let bit_depth: Handle<JsNumber> = cx.number(0);
+      let duration_in_ms: Handle<JsNumber> = cx.number(duration as f64);
+      let track_number: Handle<JsNumber> = cx.number(tag.track_number().unwrap_or(0));
+      let disc_number: Handle<JsNumber> = cx.number(tag.disc_number().unwrap_or(0));
+    }
+
+    else {
+      let tagged_file = Probe::open(path)
+      .expect("ERROR: Bad path provided!")
+      .read(true)
+      .expect("ERROR: Failed to read file!");
+
+      let tag = match tagged_file.primary_tag() {
+      Some(primary_tag) => primary_tag,
+      // If the "primary" tag doesn't exist, we just grab the
+      // first tag we can find. Realistically, a tag reader would likely
+      // iterate through the tags to find a suitable one.
+      None => tagged_file.first_tag().expect("ERROR: No tags found!"),
+      };
+      let properties: &FileProperties = tagged_file.properties();
+      let duration: u128 = properties.duration().as_millis();
+
+      let genre: Handle<JsString> = cx.string(tag.genre().unwrap_or("0"));
+      let bitrate: Handle<JsNumber> = cx.number(properties.audio_bitrate().unwrap_or(0));
+      let sample_rate: Handle<JsNumber> = cx.number(properties.sample_rate().unwrap_or(0));
+      let bit_depth: Handle<JsNumber> = cx.number(properties.bit_depth().unwrap_or(0));
+      let duration_in_ms: Handle<JsNumber> = cx.number(duration as f64);
+      let track_number: Handle<JsNumber> = cx.number(tag.track().unwrap_or(0));
+      let disc_number: Handle<JsNumber> = cx.number(tag.disk().unwrap_or(0));
+
+      if tag.pictures().len() > 0 {
+        let picture_b64: Handle<JsString> = cx.string(base64::encode(tag.pictures()[0].data()));
+        metadata_obj.set(&mut cx, "artwork", picture_b64)?;
+      }
+
+    }
+
     let metadata_obj: Handle<JsObject> = cx.empty_object();
 
     let title: Handle<JsString> = cx.string(tag.title().unwrap_or("0"));
     let artist: Handle<JsString> = cx.string(tag.artist().unwrap_or("0"));
     let album: Handle<JsString> = cx.string(tag.album().unwrap_or("0"));
-    let genre: Handle<JsString> = cx.string(tag.genre().unwrap_or("0"));
-    let bitrate: Handle<JsNumber> = cx.number(properties.audio_bitrate().unwrap_or(0));
-    let sample_rate: Handle<JsNumber> = cx.number(properties.sample_rate().unwrap_or(0));
-    let bit_depth: Handle<JsNumber> = cx.number(properties.bit_depth().unwrap_or(0));
-    let duration_in_ms: Handle<JsNumber> = cx.number(duration as f64);
     let year: Handle<JsNumber> = cx.number(tag.year().unwrap_or(0));
-    let track_number: Handle<JsNumber> = cx.number(tag.track().unwrap_or(0));
-    let disc_number: Handle<JsNumber> = cx.number(tag.disk().unwrap_or(0));
+    
     let lossless: Handle<JsBoolean> = cx.boolean(ident_lossless(file_ext.unwrap()));
     let container: Handle<JsString> = cx.string(file_ext.unwrap().to_ascii_uppercase());
     
@@ -64,17 +87,13 @@ pub fn parse_file(mut cx: FunctionContext) -> JsResult<JsObject> {
     metadata_obj.set(&mut cx, "lossless", lossless)?;
     metadata_obj.set(&mut cx, "container", container)?;
 
-    if tag.pictures().len() > 0 {
-      let picture_b64: Handle<JsString> = cx.string(base64::encode(tag.pictures()[0].data()));
-      metadata_obj.set(&mut cx, "artwork", picture_b64)?;
-    }
+   
     
     Ok(metadata_obj)
 }
 
 pub fn ident_lossless(file_ext: &str) -> bool { 
   let lowercase_ext: String = file_ext.to_ascii_lowercase();
-
 
   let result: bool = match lowercase_ext.as_str() {
     "wav" | "flac" => true,
