03ca3622a5fa63e502b8120634ac5d6a87f6ad04
GamingLiamStudios <58615717+GamingLiamStudios@users.noreply.github.com>
2022-07-16 17:00:00 +1000

    Include zlib

--- .cmake/fetch_libraries.cmake
+++ .cmake/fetch_libraries.cmake
@@ -1,10 +1,20 @@
 include(FetchContent)
 
+# Include zlib-ng
+set(ZLIB_ENABLE_TESTS OFF)
+
+FetchContent_Declare(
+    zlib
+    GIT_REPOSITORY https://github.com/zlib-ng/zlib-ng
+    GIT_TAG 2.0.6
+)
+
 # Include taglib
+# set(ZLIB_SOURCE ON)
 FetchContent_Declare(
     taglib
     GIT_REPOSITORY https://github.com/taglib/taglib
     GIT_TAG v1.12
 )
 
-FetchContent_MakeAvailable(taglib)
+FetchContent_MakeAvailable(zlib taglib)
